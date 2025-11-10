d42761248d729dac58148969917f6333f34e92e5
favourannie <javour0422@gmail.com>
2025-11-10 17:22:06 +0100

    Worked on dashboard

--- controllers/branchController.js
+++ controllers/branchController.js
@@ -3,7 +3,7 @@ const Branch = require("../models/branchModel");
 const Organization = require("../models/organizationModel");
 const { branchMail } = require("../utils/branchTemplete");
 const { sendMail } = require("../middleware/brevo");
-
+const dashboardModel = require("../models/dashboardModel")
 exports.createBranch = async (req, res) => {
   try {
     console.log('requst user:', req.user)
@@ -35,7 +35,7 @@ exports.createBranch = async (req, res) => {
       return res.status(400).json({ message: "Branch already exists" });
     }
 
-    const newBranch = await Branch.create({
+    const newBranch = new Branch ({
       organizationId: organization._id,
       branchName,
       branchCode : Math.random().toString(36).substring(2, 8).toUpperCase(),
@@ -50,7 +50,9 @@ exports.createBranch = async (req, res) => {
 
     organization.branches.push(newBranch._id);
     await organization.save();
-
+    const dashboard = await dashboardModel.create({
+     branchId: newBranch._id
+    })
    
     
     const email = organization.email || organization.email ||organization.userEmail || organization.contactEmail;
